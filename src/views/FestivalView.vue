425b9cb04b33962f8a3868c4c2855ae49fc4f0d4
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-05-30 14:15:08 +0200

    tailwind config js

--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -10,6 +10,27 @@ export default {
 <template class="bg-indigo-25">
     <header1 />
     <div class="bg-indigo-25">
+        <div class="flex justify-start items-start w-auto relative px-auto py-auto bg-gray-50 border border-white"
+>
+  <p class="flex text-red-600 text-center text-base">Histoire</p>
+  <svg
+    width="126"
+    height="54"
+    viewBox="0 0 126 54"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="flex-grow h-[53.11px] relative"
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <path
+      d="M99.7551 44.2595H26.2694C20.4716 44.2595 15.7715 42.2779 15.7715 39.8336L15.7715 13.2779C15.7715 10.8335 20.4716 8.85193 26.2694 8.85193L78.7592 8.85193C84.5571 8.85193 89.2572 10.8335 89.2572 13.2779V15.4909M99.7551 44.2595C93.9573 44.2595 89.2572 42.2779 89.2572 39.8336L89.2572 15.4909M99.7551 44.2595C105.553 44.2595 110.253 42.2779 110.253 39.8336V19.9168C110.253 17.4724 105.553 15.4909 99.7551 15.4909L89.2572 15.4909M68.2613 8.85193L47.2654 8.85193M36.7674 35.4076H68.2613M36.7674 17.7038H68.2613V26.5557H36.7674V17.7038Z"
+      stroke="#111827"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
 </div>
+    </div>
 
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,36 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        indigo: {
-          '25': '#f6f8ff',
+        "colors": {
+         "red-800": "#991b1b",
+         "Festival-info": "#f9fafb"
+        },
+        "fontSize": {
+         "sm": "0.9375rem",
+         "base": "2rem"
+        },
+        "fontFamily": {
+         "algerian": "Algerian",
+         "inter": "Inter"
+        },
+        "borderRadius": {
+         "none": "0",
+         "xs": "0.18370576202869415rem",
+         "sm": "0.25rem",
+         "default": "0.2724449634552002rem",
+         "lg": "0.3125rem",
+         "xl": "0.3508129417896271rem",
+         "2xl": "0.36270490288734436rem",
+         "3xl": "0.375rem",
+         "4xl": "0.5080958604812622rem",
+         "5xl": "0.9375rem",
+         "6xl": "1rem",
+         "7xl": "1.5625rem",
+         "8xl": "2.1875rem",
+         "9xl": "2.8271484375rem",
+         "10xl": "5.0888671875rem",
+         "full": "9999px"
         }
-      },
-      fontFamily: {
-        inter: 'Inter',
-      },
     },
   },
   plugins: [],
