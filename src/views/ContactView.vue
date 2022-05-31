2e51b33bad93cb082252a878665c8fc50e8af116
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-06-01 00:38:11 +0200

    page contact email obligatoire + conditions d'utilisations

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -13,6 +13,7 @@ export default {
     <div class="bg-indigo-25">
 <p class="flex pl-5 pt-5 pb-10 font-black text-red-800 sm:text-base lg:text-xl xl:text-2xl">Contact</p>
 <p class="flex pl-5 pt-5 pb-10 text-black sm:text-base lg:text-xl xl:text-2xl">Une question, une remarque à propos du festival ? Faites nous en part en remplissant le formulaire ci dessous :</p>
+        <form>
         <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  ">
  <div class="grid-cols-2 pl-10 pr-10 ">
      <p>Prénom*</p>
@@ -25,8 +26,8 @@ export default {
         </div>
 <br>
  <div class="grid grid-cols-1 pl-10 pr-10 ">
-     <p>Mail*</p>
-   <input type="text" class="my-6 border relative:  border-red-800" placeholder="Adresse mail">
+  <label for="emailAddress">Adresse Mail</label>
+              <input id="emailAddress" type="email" name="email" placeholder="Indiquez votre email" required class="my-6 border relative:  border-red-800">
   </div>
  <div class="grid grid-cols-1 pl-10 pr-10 ">
      <p>Objet du message*</p>
@@ -34,11 +35,15 @@ export default {
   </div>
   <div class="grid grid-cols-1 pl-10 pr-10">
       <p>Message*</p>
-<textarea id="message" name="message"  placeholder="  Message" rows="7" required class="my-3 border-red-800" ></textarea>
+<textarea class="border border-red-800" id="message" name="message"  placeholder="  Message" rows="7" required="my-3" ></textarea>
   </div>
+  <input type="checkbox" id="j'accepte" name="j'accepte"
+                            class="mr-5 h-6 w-6 border-2 border-red-800" required>
+                    <label for="j'accepte" class="check">J’accepte et j’ai  lu les conditions générales d’utilisations.</label>
   <div class="flex pl-10 pr-10 items-end">
       <button type="submit" class="rounded bg-red-800 text-white">Envoyer</button>
   </div>
+  </form>
     </div>
     <footer1 />
 
