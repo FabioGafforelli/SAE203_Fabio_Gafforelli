aeba79f6cca5c0248881e9333f60b9c5f35bcead
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-06-01 23:40:46 +0200

    création du bouton permettant de changer l'image de la card dans la page modifierview.vue

---
+++ src/components/icones/modifierListe/BoutonImage.vue
@@ -0,0 +1,10 @@
+<template>
+    <button class="bg-white text-black rounded-xl p-2 px-1 font-bold w-1/2 text-center">{{ Nom }}</button> 
+</template>
+<script>
+export default{
+    props : {
+        Nom: String,
+    }
+};
+</script>
