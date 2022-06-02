9b5c3586b3934307623e7144f8948cc7872fd67c
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-06-02 15:02:08 +0200

    ajout de la police d'écriture sur le bouton présent sur

--- src/components/icones/modifierListe/BoutonImage.vue
+++ src/components/icones/modifierListe/BoutonImage.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="bg-white text-black rounded-xl p-2 px-1 font-bold w-1/2 text-center">{{ Nom }}</button> 
+    <button class="bg-red-800 text-white font-algerian font-bold w-1/2 text-center">{{ Nom }}</button> 
 </template>
 <script>
 export default{
